bbe7e8b34334ed84d045afbb14fa1a79de60ad7a
anaeitan14 <eitanlich@yahoo.com>
2021-04-21 20:30:01 +0300

    test 21/4/21

---
+++ solution/src/App.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class Solution { 
+
+    public static void main(String[] args) { 
+    
+        Scanner in = new Scanner(System.in);
+        
+        
+        int numberOfElements = in.nextInt();                                                 
+        
+        int array[] = new int[numberOfElements];                                                     
+        
+        int sumOfArray = 0; loop
+        
+        for(int i = 0; i < numberOfElements; i++) { 
+            
+        
+            array[i] = in.nextInt(); 
+            sumOfArray += array[i]; 
+        }
+        System.out.println(sumOfArray);
+    }
+}
